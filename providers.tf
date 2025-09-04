8efdc0fc370066136dc68f1c7190440bed9cdc82
aracloud <aracloud@gmx.net>
2025-09-04 16:35:23 +0200

    change

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    volterra = {
+      source = "volterraedge/volterra"
+      version = ">=0.0.6"
+    }
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "~> 2.9.11"
+    }
+  }
+}
+
+
+
+provider "volterra" {
+  // api_p12_file = var.f5xc_api_p12_file
+  // url          = var.f5xc_api_url
+  // alias        = "default"
+}
+
