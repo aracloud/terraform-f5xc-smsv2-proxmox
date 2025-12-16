456e0eeb7ecdaa6b73ba1d6b685fd27424eaa7ab
aracloud <aracloud@gmx.net>
2025-12-16 17:20:36 +0100

    change

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "volterra" {
-  // api_p12_file = var.f5xc_api_p12_file
-  // url          = var.f5xc_api_url
-  // timeout      = "30s"
+  api_p12_file = var.f5xc_api_p12_file
+  url          = var.f5xc_api_url
+  timeout      = "30s"
 }
 
 provider "proxmox" {
