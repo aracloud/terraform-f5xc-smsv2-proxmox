83ae9b9733ae4e22bbe65aad27d3d019c44e0a82
ndee <ndee@dkr01.loc.ndee.ch>
2026-08-20 11:07:34 +0200

    change

--- output.tf
+++ output.tf
@@ -1,9 +1,6 @@
 output "proxmox" {
   value = {
-    securemesh = {
-      id = odule.securemesh-v2-site.id
-      name = odule.securemesh-v2-site.name
-    }
+    securemesh = module.securemesh-v2-site
   }
   sensitive = true
 }
